c40c6993ab2f9f167dd2e9a0713441f55107f261
Ferry Aditya H <ferryaditya124@gmail.com>
2023-06-12 06:32:03 +0700

    #logic build

--- ProyekAkhir/src/main.cpp
+++ ProyekAkhir/src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <DHT.h>
 
-#define RELAY_PIN 12   // Pin relay terhubung ke GPIO 12
-#define MQ135_PIN A0   // Pin ADC untuk sensor MQ-135
-#define MQ2_PIN A1     // Pin ADC untuk sensor MQ-2
-#define DHT_PIN 4      // Pin data sensor DHT22 terhubung ke GPIO 4
+#define RELAY_PIN 4    // Pin relay terhubung ke GPIO 12
+#define MQ135_PIN 34   // Pin ADC untuk sensor MQ-135
+#define MQ2_PIN 35     // Pin ADC untuk sensor MQ-2
+#define DHT_PIN 2      // Pin data sensor DHT22 terhubung ke GPIO 4
 #define DHT_TYPE DHT22 // Tipe sensor DHT22, ganti menjadi DHT11 jika menggunakan sensor DHT11
-#define RED_PIN 12     // Pin GPIO yang terhubung ke kaki merah LED RGB
-#define GREEN_PIN 13   // Pin GPIO yang terhubung ke kaki hijau LED RGB
-#define BLUE_PIN 14    // Pin GPIO yang terhubung ke kaki biru LED RGB
+#define RED_PIN 21     // Pin GPIO yang terhubung ke kaki merah LED RGB
+#define GREEN_PIN 19   // Pin GPIO yang terhubung ke kaki hijau LED RGB
+#define BLUE_PIN 18    // Pin GPIO yang terhubung ke kaki biru LED RGB
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
@@ -26,12 +26,19 @@ void readmq()
   delay(1000); // Jeda 1 detik sebelum membaca nilai sensor lagi
 }
 
-void relay()
+void relay(int nilai)
 {
-  digitalWrite(RELAY_PIN, HIGH); // Menghidupkan relay
-  delay(2000);                   // Menunggu selama 2 detik
-  digitalWrite(RELAY_PIN, LOW);  // Mematikan relay
-  delay(2000);                   // Menunggu selama 2 detik
+  if (nilai == 1)
+  {
+    Serial.println("Kipas Menyala");
+    digitalWrite(RELAY_PIN, HIGH); // Menghidupkan relay
+    delay(2000);                   // Menunggu selama 2 detik
+  }
+  else
+  {
+    digitalWrite(RELAY_PIN, LOW); // Mematikan relay
+    delay(2000);                  // Menunggu selama 2 detik
+  }
 }
 
 void readdht22()
@@ -50,32 +57,26 @@ void readdht22()
   Serial.println(" %");
 }
 
-void lamprgb()
+void lamprgb(int led)
 {
-  // Merah
-  digitalWrite(RED_PIN, HIGH);
-  digitalWrite(GREEN_PIN, LOW);
-  digitalWrite(BLUE_PIN, LOW);
-  delay(1000);
 
-  // Hijau
-  digitalWrite(RED_PIN, LOW);
-  digitalWrite(GREEN_PIN, HIGH);
-  digitalWrite(BLUE_PIN, LOW);
-  delay(1000);
-
-  // Biru
-  digitalWrite(RED_PIN, LOW);
-  digitalWrite(GREEN_PIN, LOW);
-  digitalWrite(BLUE_PIN, HIGH);
-  delay(1000);
+  if (led == 1) // status bahaya
+  {
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(BLUE_PIN, LOW);
+  }
+  else if (led == 2) // status aman
+  {
+    digitalWrite(RED_PIN, LOW);
+    digitalWrite(GREEN_PIN, HIGH);
+    digitalWrite(BLUE_PIN, LOW);
+  }
 }
 
-int myFunction(int, int);
 void setup()
 {
-
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT); // Mengatur pin relay sebagai output
   pinMode(MQ135_PIN, INPUT);  // Mengatur pin sensor MQ-135 sebagai input
   pinMode(MQ2_PIN, INPUT);    // Mengatur pin sensor MQ-2 sebagai input
@@ -83,13 +84,53 @@ void setup()
   pinMode(RED_PIN, OUTPUT);   // Mengatur pin merah sebagai output
   pinMode(GREEN_PIN, OUTPUT); // Mengatur pin hijau sebagai output
   pinMode(BLUE_PIN, OUTPUT);  // Mengatur pin biru sebagai output
+  digitalWrite(GREEN_PIN, HIGH);
+}
+
+void qualityair()
+{
+  int mq135Value = analogRead(MQ135_PIN); // Membaca nilai dari sensor MQ-135
+
+  float o2Concentration = map(mq135Value, 0, 1023, 0, 100);  // Mengkonversi nilai menjadi konsentrasi O2
+  float co2Concentration = map(mq135Value, 0, 1023, 100, 0); // Mengkonversi nilai menjadi konsentrasi CO2
+
+  Serial.print("Konsentrasi O2: ");
+  Serial.print(o2Concentration);
+  Serial.println(" %");
+
+  Serial.print("Konsentrasi CO2: ");
+  Serial.print(co2Concentration);
+  Serial.println(" %");
+
+  if (co2Concentration > 1000)
+  {
+    Serial.println("Peringatan: Konsentrasi CO2 tinggi");
+    relay(1);   // menyalakan relay
+    lamprgb(1); // led merah
+  }
+  else
+  {
+    relay(0);   // mematikan relay
+    lamprgb(2); // led hijau
+  }
+
+  delay(2000);
+}
+
+void readmq2()
+{
+  int mq2Value = analogRead(MQ2_PIN);
+  if (mq2Value > 100)
+  {
+    Serial.println("Deteksi Asap Rokok");
+    lamprgb(1);
+    // Lakukan tindakan atau tampilkan notifikasi sesuai kebutuhan Anda
+  }
 }
 
 void loop()
 {
-}
-
-int myFunction(int x, int y)
-{
-  return x + y;
+  qualityair();
+  readmq2();
+  readdht22();
 }
